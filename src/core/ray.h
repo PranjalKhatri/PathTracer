d2544582111fecbca412414649b3a56b7f409ad2
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-05-27 23:20:26 +0530

    Added ray class and CMakeLists.txt

---
+++ src/core/ray.h
@@ -0,0 +1,19 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "math/vec3.h"
+
+class ray {
+   public:
+    ray() {}
+    ray(const point3& origin, const vec3& direction)
+        : m_orig(origin), m_dir(direction) {}
+    const point3& origin() const { return m_orig; }
+    const vec3& direction() const { return m_dir; }
+    point3 at(double t) const { return m_orig + t * m_dir; }
+
+   private:
+    point3 m_orig;
+    vec3 m_dir;
+};
+#endif
