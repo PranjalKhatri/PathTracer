1e9b0a7b89b88b39cfe71b062175cbc2039d0043
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-05-27 23:20:02 +0530

    Added vec3 and color class in maths

---
+++ src/math/color.h
@@ -0,0 +1,21 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+
+//(r,g,b): r,g,b E [0,1]
+using color = vec3;
+
+void write_color(std::ostream& out, const color& pixel_color) {
+    auto r = pixel_color.x();
+    auto g = pixel_color.x();
+    auto b = pixel_color.x();
+
+    int rbyte = static_cast<int>(255.999 * r);
+    int gbyte = static_cast<int>(255.999 * g);
+    int bbyte = static_cast<int>(255.999 * b);
+
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+#endif
