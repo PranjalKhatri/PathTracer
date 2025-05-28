e79d62a480eb366a5441cd5d8e7916356807158b
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-05-28 23:39:37 +0530

    separated constants from rtweekend

---
+++ src/util/constants.h
@@ -0,0 +1,10 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <limits>
+
+//  Constants
+constexpr double infinity = std::numeric_limits<double>::infinity();
+constexpr double pi = 3.1415926535897932385;
+
+#endif
