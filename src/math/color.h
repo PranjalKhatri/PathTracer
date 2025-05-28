7e592b2e0b3b194cae52f3585de8338d786f66a0
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-05-28 14:47:34 +0530

    inlined writecolor()

--- src/math/color.h
+++ src/math/color.h
@@ -6,7 +6,7 @@
 //(r,g,b): r,g,b E [0,1]
 using color = vec3;
 
-void write_color(std::ostream& out, const color& pixel_color) {
+inline void write_color(std::ostream& out, const color& pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
