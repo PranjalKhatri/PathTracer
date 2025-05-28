5539fcf5627a5ef5dccafd693151a73b3cb72708
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-05-28 15:32:26 +0530

    made constants constexpr

--- src/util/rtweekend.h
+++ src/util/rtweekend.h
@@ -1,5 +1,5 @@
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
 
 #include <cmath>
 #include <iostream>
@@ -12,8 +12,8 @@ using std::shared_ptr;
 using std::sqrt;
 
 //  Constants
-const double infinity = std::numeric_limits<double>::infinity();
-const double pi = 3.1415926535897932385;
+constexpr double infinity = std::numeric_limits<double>::infinity();
+constexpr double pi = 3.1415926535897932385;
 
 //  Utility Functions
 inline double degrees_to_radians(double degrees) {
