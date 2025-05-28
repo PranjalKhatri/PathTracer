1cb562ff988931096a77b262d82e052b93325eec
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-05-28 13:54:40 +0530

    basic sphere with normals

--- src/main.cxx
+++ src/main.cxx
@@ -1,10 +1,35 @@
 #include <iostream>
 
 #include "core/ray.h"
+#include "hittable/hittable.h"
 #include "math/color.h"
 #include "math/vec3.h"
 
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = r.direction().length_squared();
+    auto h = dot(r.direction(), oc);
+    auto c = oc.length_squared() - radius * radius;
+    //  auto b = -2.0 * dot(r.direction(), oc);
+    //  auto discriminant = b * b - 4 * a * c;
+    auto discriminant = h * h - a * c;
+
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (h - sqrt(discriminant)) / a;
+        //  return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
+}
+
 color ray_color(const ray& r) {
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N{unit_vector(r.at(t) -
+                           vec3(0, 0, -1))};    //  from centre to hit point
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
+
     vec3 unit_direction{unit_vector(r.direction())};
     auto a = 0.5 * (unit_direction.y() + 1.0);    //  scale [-1,1] -> [0,1]
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
