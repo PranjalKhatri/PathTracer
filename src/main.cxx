d0e2fbd70637078491afbf46f8d0716f6c1fd7aa
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-05-29 22:21:14 +0530

    added a scene showing vfov

--- src/main.cxx
+++ src/main.cxx
@@ -7,7 +7,7 @@
 #include "hittable/sphere.h"
 #include "materials/dielectric.h"
 
-void metal_spheres(hittable_list& world) {
+void metal_spheres(hittable_list& world, camera& cam) {
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
     auto material_left = make_shared<dielectric>(1.50);
@@ -21,19 +21,41 @@ void metal_spheres(hittable_list& world) {
     world.add(
         make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, material_bubble));
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
-}
-
-int main() {
-    hittable_list world;
-
-    metal_spheres(world);
-
-    camera cam;
 
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth = 50;
+}
+void distant_view(hittable_list& world, camera& cam) {
+    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left = make_shared<dielectric>(1.50);
+    auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+    world.add(
+        make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(
+        make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(
+        make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, material_bubble));
+    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+    cam.vfov = 90;
+    cam.lookfrom = point3(-2, 2, 1);
+    cam.lookat = point3(0, 0, -1);
+    cam.vup = vec3(0, 1, 0);
+}
+int main() {
+    hittable_list world;
 
+    camera cam;
+
+    //  metal_spheres(world,cam);
+    distant_view(world, cam);
     cam.render(world);
 }
