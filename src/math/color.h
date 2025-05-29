a826062a5a1d7807f84d6a3705cc235e39c9e87a
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-05-29 16:04:08 +0530

    added gamma correction

--- src/math/color.h
+++ src/math/color.h
@@ -13,12 +13,19 @@ struct ColorConstants {
     static constexpr vec3 GREEN{0.0, 1.0, 0.0};
     static constexpr vec3 BLUE{0.0, 0.0, 1.0};
 };
-
+inline double linear_to_gamma(double linear_component) {
+    if (linear_component > 0) return sqrt(linear_component);
+    return 0.0;
+}
 inline void write_color(std::ostream& out, const color& pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
 
+    //  linear to gamma tranform for gamma 2
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
     //  Translate the [0,1] component values to the byte range [0,255].
     static const interval intensity(0.000, 0.999);
     int rbyte = static_cast<int>(256 * intensity.clamp(r));
