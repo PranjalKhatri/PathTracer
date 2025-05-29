7113dc7af51723cb85f9802eff30a64e9496bde8
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-05-29 21:03:04 +0530

    added fuzziness

--- src/materials/metal.h
+++ src/materials/metal.h
@@ -6,17 +6,20 @@
 
 class metal : public material {
    public:
-    metal(const color& albedo) : m_albedo(albedo) {}
+    metal(const color& albedo, double fuzz)
+        : m_albedo(albedo), m_fuzz(fuzz < 1 ? fuzz : 1) {}
     bool scatter(const ray& r_in, const hit_record& rec, color& attenuation,
-                 ray& scattered)const override {
+                 ray& scattered) const override {
         vec3 reflected = reflect(r_in.direction(), rec.normal);
+        reflected = unit_vector(reflected) + (m_fuzz * random_unit_vector());
         scattered = ray(rec.hit_point, reflected);
         attenuation = m_albedo;
-        return true;
+        return (dot(scattered.direction(), rec.normal) > 0);
     }
 
    private:
     color m_albedo;
+    double m_fuzz;
 };
 
 #endif
