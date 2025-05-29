1bfc18fd39d31a8a2346879a133cd28df8913b68
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-05-29 21:33:11 +0530

    used schliks approx for reflactance

--- src/materials/dielectric.h
+++ src/materials/dielectric.h
@@ -14,6 +14,17 @@ class dielectric : public material {
         double ri = rec.front_face ? (1.0 / m_refraction_index)
                                    : m_refraction_index;
         vec3 unit_direction = unit_vector(r_in.direction());
+        //  total internal reflection
+        double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+        double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+        bool cannot_refract = ri * sin_theta > 1.0;
+        vec3 direction;
+        if (cannot_refract || reflectance(cos_theta, ri) > random_double())
+            direction = reflect(unit_direction, rec.normal);
+        else
+            direction = refract(unit_direction, rec.normal, ri);
+        scattered = ray(rec.hit_point, direction);
+
         vec3 refracted = refract(unit_direction, rec.normal, ri);
         scattered = ray(rec.hit_point, refracted);
         return true;
@@ -23,6 +34,13 @@ class dielectric : public material {
     //  Refractive index in vacuum or air, or the ratio of the material's
     //  refractive index over the refractive index of the enclosing media
     double m_refraction_index;
+
+    static double reflectance(double cosine, double refraction_index) {
+        //  Using Schlick's approximation for reflectance.
+        auto r0 = (1 - refraction_index) / (1 + refraction_index);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * pow((1 - cosine), 5);
+    }
 };
 
 #endif
