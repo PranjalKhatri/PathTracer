1dc1e65832283fdd6b3bfecba20a4ecd9b61603b
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-05-30 20:57:27 +0530

    checker test

--- cpu/src/materials/lambertian.h
+++ cpu/src/materials/lambertian.h
@@ -4,9 +4,13 @@
 #include "hittable/hittable.h"
 #include "material.h"
 
+#include "textures/texture.h"
+#include "materials/textures/solid_color.h"
+
 class lambertian : public material {
    public:
-    lambertian(const color& albedo) : m_albedo(albedo) {}
+    lambertian(const color& albedo) : m_tex(make_shared<solid_color>(albedo)) {}
+    lambertian(shared_ptr<texture> tex) : m_tex(tex) {}
     //  TODO: test with reflectance R
     bool scatter(const ray& r_in, const hit_record& rec, color& attenuation,
                  ray& scattered) const override {
@@ -14,12 +18,13 @@ class lambertian : public material {
         //  Catch degenerate scatter direction
         if (scatter_direction.near_zero()) scatter_direction = rec.normal;
         scattered = ray(rec.hit_point, scatter_direction, r_in.time());
-        attenuation = m_albedo;
+        attenuation = m_tex->value(rec.u, rec.v, rec.hit_point);
         return true;
     }
 
    private:
-    color m_albedo;
+    //  color m_albedo;
+    shared_ptr<texture> m_tex;
 };
 
 #endif
--- cpu/src/scenes/scene.h
+++ cpu/src/scenes/scene.h
@@ -11,6 +11,7 @@
 #include "materials/metal.h"
 #include "hittable/sphere.h"
 #include "materials/dielectric.h"
+#include "materials/textures/checker.h"
 
 inline void metal_spheres(hittable_list& world, camera& cam) {
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
@@ -58,8 +59,13 @@ inline void distant_view(hittable_list& world, camera& cam) {
     cam.focus_dist = 3.4;
 }
 inline void main_scene(hittable_list& world, camera& cam) {
-    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+    //  auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    //  world.add(make_shared<sphere>(point3(0, -1000, 0), 1000,
+    //  ground_material));
+    auto checker = make_shared<checker_texture>(0.32, color(.2, .3, .1),
+                                                color(.9, .9, .9));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000,
+                                  make_shared<lambertian>(checker)));
     for (int a = -11; a < 11; a++) {
         for (int b = -11; b < 11; b++) {
             auto choose_mat = random_double();
@@ -101,8 +107,8 @@ inline void main_scene(hittable_list& world, camera& cam) {
 
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = 400;
-    cam.samples_per_pixel = 10;
-    cam.max_depth = 10;
+    cam.samples_per_pixel = 20;
+    cam.max_depth = 20;
     cam.vfov = 20;
     cam.lookfrom = point3(13, 2, 3);
     cam.lookat = point3(0, 0, 0);
