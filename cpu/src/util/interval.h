0aa656261fbabd9c8b9f40d4e08d6bb1c652736c
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-05-30 12:56:17 +0530

    added enclosing interval functions

--- cpu/src/util/interval.h
+++ cpu/src/util/interval.h
@@ -11,6 +11,11 @@ class interval {
     interval()
         : min(+infinity), max(-infinity) {}    //  Default interval is empty
     interval(double min, double max) : min(min), max(max) {}
+    //  Create the interval tightly enclosing the two input intervals.
+    interval(const interval& a, const interval& b) {
+        min = a.min <= b.min ? a.min : b.min;
+        max = a.max >= b.max ? a.max : b.max;
+    }
     double size() const { return max - min; }
     bool contains(double x) const { return min <= x && x <= max; }
     bool surrounds(double x) const { return min < x && x < max; }
