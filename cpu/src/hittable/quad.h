8030bbe3caf50d33b6a4d253ba08f9fd482b3850
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-05-31 15:15:16 +0530

    implemented quad uv coords

--- cpu/src/hittable/quad.h
+++ cpu/src/hittable/quad.h
@@ -13,6 +13,7 @@ class quad : public hittable {
         : Q(Q), u(u), v(v), mat(mat) {
         auto n = cross(u, v);
         normal = unit_vector(n);
+        w = n / dot(n, n);
         D = dot(normal, Q);
 
         set_bounding_box();
@@ -24,13 +25,46 @@ class quad : public hittable {
         bbox = aabb(bbox_diagonal1, bbox_diagonal2);
     }
     aabb bounding_box() const override { return bbox; }
+
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-        return false;    //  To be implemented
+        auto denom = dot(normal, r.direction());
+        //  No hit if the ray is parallel to the plane.
+        if (fabs(denom) < 1e-8) return false;
+        //  Return false if the hit point parameter t is outside the ray
+        //  interval.
+        auto t = (D - dot(normal, r.origin())) / denom;
+        if (!ray_t.contains(t)) return false;
+        //  Determine the hit point lies within the planar shape using its plane
+        //  coordinates.
+        auto intersection = r.at(t);
+        vec3 planar_hitpt_vector = intersection - Q;
+        auto alpha = dot(w, cross(planar_hitpt_vector, v));
+        auto beta = dot(w, cross(u, planar_hitpt_vector));
+        if (!is_interior(alpha, beta, rec)) return false;
+        //  Ray hits the 2D shape; set the rest of the hit record and return
+        //  true
+        rec.t = t;
+        rec.hit_point = intersection;
+        rec.mat = mat;
+        rec.set_face_normal(r, normal);
+        return true;
+    }
+    //  Given the hit point in plane coordinates, return false if it is
+    //  outside the primitive, otherwise set the hit record UV coordinates
+    //  and return true.
+    virtual bool is_interior(double a, double b, hit_record& rec) const {
+        interval unit_interval = interval(0, 1);
+        if (!unit_interval.contains(a) || !unit_interval.contains(b))
+            return false;
+        rec.u = a;
+        rec.v = b;
+        return true;
     }
 
    private:
     point3 Q;                    //  one vertex of quad
     vec3 u, v;                   //  vector to adjacent vertices of Q
+    vec3 w;                      //  n/(n.(uXv))
     shared_ptr<material> mat;    //  materail
     aabb bbox;
     vec3 normal;    //  normal to quad (uXv)
