1d485a41451164848c57351cbdd0067ebcae270b
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-05-31 12:37:11 +0530

    Added phase to perlin texture

--- cpu/src/materials/textures/noise_texture.h
+++ cpu/src/materials/textures/noise_texture.h
@@ -6,12 +6,14 @@
 
 class noise_texture : public texture {
    public:
-    noise_texture() : m_scale(1.0), m_turb(1) {}
+    noise_texture() : m_scale(1.0), m_turb(7) {}
 
-    noise_texture(double scale, int turb = 1) : m_scale(scale), m_turb(turb) {}
+    noise_texture(double scale, int turb = 7) : m_scale(scale), m_turb(turb) {}
     color value(double u, double v, const point3& p) const override {
-        return ColorConstants::WHITE * m_noise.turb(p, m_turb);
-        //  return color(1, 1, 1) * 0.5 * (1.0 + m_noise.noise(m_scale * p));
+        return color(.5, .5, .5) *
+               (1 + sin(m_scale * p.z() + 10 * m_noise.turb(p, m_turb)));
+        //  return ColorConstants::WHITE * m_noise.turb(p, m_turb);
+        //   return color(1, 1, 1) * 0.5 * (1.0 + m_noise.noise(m_scale * p));
     }
 
    private:
